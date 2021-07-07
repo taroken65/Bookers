ed4aabfe9f6a17693ca3f88463951aeea67898b8
taroken65 <yonemurano@gmail.com>
2021-07-07 07:13:31 +0000

    0707 commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,6 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
-    @books = Book.all.order(created_at: :desc)
     @book = Book.new
   end
 
@@ -9,15 +8,16 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
-  def new
-    @book = Book.new
-  end
-
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
-    flash[:notice] = "Book was successfully updated."
+    @book = Book.new(book_params)
+    if @book.save
+    flash[:notice] = "successfully"
+    redirect_to book_path(@book.id)
+    else
+    @books = Book.all
+    flash[:error] = "error"
+    render("books/index")
+    end
   end
 
   def edit
@@ -25,17 +25,21 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
-    flash[:notice] = "Book was successfully updated."
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    flash[:notice] = "successfully"
+    redirect_to book_path(@book.id)
+    else
+    flash[:error2] = "error"
+    render("edit")
+    end
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to books_path
     flash[:notice] = "Book was successfully destroyed."
+    redirect_to books_path
   end
 
  private
